99b4ba12b4c6699bf67eb5a28e549aba968b5045
okm547896321 <chenl_work@163.com>
2018-12-09 18:12:15 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # chromeExtensionSimpleTranslate
-simple translate -google网页翻译
+simple translate - google网页翻译
+
+在新标签页中打开google网页翻译界面：https://translate.google.com/translate?...
---
+++ simpleTranslate/popup.js
@@ -0,0 +1,13 @@
+﻿
+document.addEventListener('DOMContentLoaded', function() {
+    
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	var url = (tab[0]||tab).url;
+	// console.info("aaa", url, tab);
+    chrome.tabs.create({
+        selected: true,
+        url: "https://translate.google.com/translate?sl=auto&tl=zh-CN&js=y&prev=_t&hl=zh-CN&ie=UTF-8&u=" + url + "&edit-text=&act=url"
+    });
+});
